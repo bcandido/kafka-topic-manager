87247a25eb7d6604ed4b104236518408266f9fd3
Brenno Candido <bcandido.bc@gmail.com>
2020-06-05 19:25:24 -0300

    wip

---
+++ pkg/apis/kafka/v1/kafkatopic_types.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KafkaTopic
+// +k8s:openapi-gen=true
+// +resource:path=kafkatopics,strategy=KafkaTopicStrategy
+type KafkaTopic struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KafkaTopicSpec   `json:"spec,omitempty"`
+	Status KafkaTopicStatus `json:"status,omitempty"`
+}
+
+// KafkaTopicSpec defines the desired state of KafkaTopic
+type KafkaTopicSpec struct {
+	Partitions int `json:",inline"`
+	Replicas   int `json:",inline"`
+}
+
+// KafkaTopicStatus defines the observed state of KafkaTopic
+type KafkaTopicStatus struct {
+}
